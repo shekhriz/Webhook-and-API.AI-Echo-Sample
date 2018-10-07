48131d29f0e0dc4c367dc261d2b47cb72a9f282b
Shekh Rizwan <shekh.rizwan@accionlabs.com>
2018-10-07 12:05:52 +0530

    added code

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ restService.post("/echo", function(req, res) {
       ? req.body.result.parameters.echoText
       : "Seems like some problem. Speak again.";
   return res.json({
-    speech: 'Rizwan Bhai',
-    displayText: 'Rizwan Bhai',
+    speech: speech,
+    displayText: speech,
     source: "webhook-echo-sample"
   });
 });
